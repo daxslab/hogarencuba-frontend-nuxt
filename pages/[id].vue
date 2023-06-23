a5d7ebd5038f409d4addda467921cedd7fff52e6
Gabriel A. López López <glpz@daxslab.com>
2023-06-23 18:15:09 -0400

    Create real estate details page

---
+++ pages/[id].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+const config = useRuntimeConfig()
+const route = useRoute()
+
+let state = reactive({
+  item: {}
+})
+
+onMounted(async () => {
+  console.log(route.params.id);
+  const response = await fetch(`${config.public.HEC_API_HOST}/real-estates/${route.params.id}`)
+  state.item = await response.json()
+  console.log(state.items)
+})
+
+</script>
+
+<template>
+  {{ JSON.stringify(state.item) }}
+</template>
+
+<style scoped>
+
+</style>
