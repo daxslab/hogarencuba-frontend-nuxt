75684523a276bdbb4b2298d9bed93c53c549471a
Gabriel A. López López <glpz@daxslab.com>
2023-06-23 21:38:06 -0400

    Add vuetify module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,23 @@ export default defineNuxtConfig({
             HEC_API_HOST: process.env.HEC_API_HOST,
         }
     },
+    modules: [
+        '@invictus.codes/nuxt-vuetify'
+    ],
+    vuetify: {
+        /* vuetify options */
+        vuetifyOptions: {
+            // @TODO: list all vuetify options
+        },
+
+        moduleOptions: {
+            /* nuxt-vuetify module options */
+            treeshaking: true,
+            useIconCDN: true,
+
+            /* vite-plugin-vuetify options */
+            styles: true,
+            autoImport: true,
+        }
+    }
 })
