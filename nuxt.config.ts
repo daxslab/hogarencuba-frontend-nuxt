a8a773f40f1f5cff672aa8345f6156ecf2a43bdd
Gabriel A. López López <glpz@daxslab.com>
2023-06-24 11:32:59 -0400

    Setup page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' }
+    },
     runtimeConfig: {
         public: {
             HEC_API_HOST: process.env.HEC_API_HOST,
--- pages/[id].vue
+++ pages/[id].vue
@@ -8,27 +8,30 @@ let state = reactive({
 })
 
 onMounted(async () => {
-  console.log(route.params.id);
   const response = await fetch(`${config.public.HEC_API_HOST}/real-estates/${route.params.id}?expand=images,related`)
   state.item = await response.json()
-  console.log(state.item)
 })
 
 </script>
 
 <template>
-  <v-row v-if="state.item">
-    <v-col cols="8">
-      <RealEstateItem :item="state.item" :full="1"/>
-    </v-col>
-    <v-col cols="4">
-      <SiteContactForm :item="state.item" />
-    </v-col>
-  </v-row>
-  <section v-if="state.item && state.item.related.length">
-    <h2>Similar properties</h2>
-    <RealEstateList :items="state.item.related"/>
-  </section>
+  <article>
+    <v-row v-if="state.item">
+      <v-col cols="8">
+        <RealEstateItem :item="state.item" :full="1"/>
+      </v-col>
+      <v-col cols="4">
+        <SiteContactForm :item="state.item"/>
+      </v-col>
+      <v-col v-if="state.item && state.item.related.length" tag="section" cols="12">
+        <h2>Similar properties</h2>
+        <RealEstateList :items="state.item.related"/>
+      </v-col>
+      <p v-else>
+        Loading...
+      </p>
+    </v-row>
+  </article>
 </template>
 
 <style scoped>
