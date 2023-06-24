5090bc878b9faf6a29a766f74e4ac4e79082e073
Gabriel A. López López <glpz@daxslab.com>
2023-06-23 21:39:06 -0400

    Reuse the RealEstateItem in the real estate details page

--- pages/[id].vue
+++ pages/[id].vue
@@ -4,20 +4,31 @@ const config = useRuntimeConfig()
 const route = useRoute()
 
 let state = reactive({
-  item: {}
+  item: null,
 })
 
 onMounted(async () => {
   console.log(route.params.id);
-  const response = await fetch(`${config.public.HEC_API_HOST}/real-estates/${route.params.id}`)
+  const response = await fetch(`${config.public.HEC_API_HOST}/real-estates/${route.params.id}?expand=images,related`)
   state.item = await response.json()
-  console.log(state.items)
+  console.log(state.item)
 })
 
 </script>
 
 <template>
-  {{ JSON.stringify(state.item) }}
+  <v-row v-if="state.item">
+    <v-col cols="8">
+      <RealEstateItem :item="state.item" :full="1"/>
+    </v-col>
+    <v-col cols="4">
+      The contact form
+    </v-col>
+  </v-row>
+  <section v-if="state.item && state.item.related.length">
+    <h2>Similar properties</h2>
+    <RealEstateList :items="state.item.related"/>
+  </section>
 </template>
 
 <style scoped>
